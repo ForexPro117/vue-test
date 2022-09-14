ab0e1508e665544a225dfa736f4bca779176b104
ForexPro <roma.minihanowa@mail.ru>
2022-09-14 20:35:20 +0400

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,19 @@
 </template>
 
 <script>
+import axios from 'axios'
   export default {
     name: 'HelloWorld',
 
     data(){
       return {
 
+      }
+    },
+    methods:{
+      addWorkers(){
+        axios.get("http://localhost:8080/add",{name:"worker",position:"prorab"})
+        .then(()=> console.log("успех"))
       }
     },
   }
