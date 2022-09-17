dc554d870d59b09b9de3de1eb38923360e2d197e
MyTh1c2L <egor.esenkulov.90@mail.ru>
2022-09-17 22:40:15 +0400

    delete worker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
 div
+  v-text-field(v-model="uuid" placeholder="Введите UUID рабочего, которого необходимо удалить") 
+  v-btn(@click="deleteWorker") Удалить рабочего
+
   div.mt-5(v-for="worker in workers")
     div имя: {{worker.name}} должность: {{worker.position}} код: {{worker.uuid}} 
 </template>
@@ -11,7 +14,8 @@ import axios from 'axios'
 
     data(){
       return {
-        workers: []
+        workers: [],
+        uuid: '',
       }
     },
     mounted(){
@@ -23,14 +27,12 @@ import axios from 'axios'
         axios.get("http://localhost:8080/get")
         .then((data) => {
           this.workers = data.data
-          this.getWorker()
         })
       },
-      getWorker(){
-        axios.get(`http://localhost:8080/get/${this.workers[0].uuid}`)
-        .then((data) => {
-          console.log("you first worker", data.data);
-        })
+      deleteWorker(){
+        axios.get(`http://localhost:8080/delete/${this.uuid}`)
+        .then(() => console.log("успех")),
+        this.getWorkers()
       }
     },
 
