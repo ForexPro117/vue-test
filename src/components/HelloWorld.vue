88d33a81f6a84a43657fc0a8e894f1d7f9cabf09
MyTh1c2L <egor.esenkulov.90@mail.ru>
2022-09-23 13:13:02 +0400

    Изменение и удаление работника

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,21 +2,34 @@
 div
   v-data-table.mt-10.table(:headers="headers" :items="workers" style="width:1000px")
     template(v-slot:item.actions="{ item }")
-      v-icon(small @click="dialog=true") mdi-pencil
+      v-icon(small @click="editWorker(item)") mdi-pencil
       v-icon(small @click="deleteWorker(item)") mdi-delete
 
-
-
-
-  v-dialog(v-model="dialog" width="500")
+  v-dialog(v-model="deleteDialog" width="800")
     v-card
-      v-card-title За шо менять
-      v-card-text Тут ты типо выводишт свой обект по частям, т.е. тебе над передавать объектики
+      v-card-title(class="text-h5") Вы уверены, что хотите удалить данные об этом работнике?
+      v-divider
+      v-card-actions 
+        v-btn( color="red" text @click="deleteDialog=false") отмена
+          v-icon(right) mdi-close
+        v-spacer
+        v-btn( color="teal accent-4" text @click="deleteWorkerCofirm()") удалить
+          v-icon(right) mdi-delete
+
+
+  v-dialog(v-model="editDialog" width="500")
+    v-card
+      v-card-title(class="text-h5") Изменение данных работника
+      v-card-text ФИО
+        v-text-field(v-model="name")
+      v-card-text Должность
+        v-text-field(v-model="position")
       v-divider
       v-card-actions
-        v-btn( color="red" text @click="dialog=false") отмена
+        v-btn( color="red" text @click="editDialog=false") отмена
+          v-icon(right) mdi-close
         v-spacer
-        v-btn( color="teal accent-4" text) изменить
+        v-btn( color="teal accent-4" text @click="editWorkerConfirm()") изменить
           v-icon(right) mdi-pencil     
 </template>
 
@@ -32,8 +45,13 @@ import axios from 'axios'
           { text: 'Должность', value: 'position' },
           { text: 'Действия', value: 'actions', sortable: false }
         ],
-        dialog:false,
-        workers: []
+        deleteDialog:false,
+        editDialog:false,
+        deleteUUID:-1,
+        uuid: '',
+        name: '',
+        position: '',
+        workers: [],
       }
     },
     mounted(){
@@ -45,13 +63,35 @@ import axios from 'axios'
         axios.get("http://localhost:8080/get")
         .then((data) => { this.workers = data.data })
       },
-      deleteWorker(item){
-        axios.delete(`http://localhost:8080/delete/${this.workers[this.workers.indexOf(item)]}`)
-        .then(() => this.getWorkers())
+      deleteWorkerCofirm(){
+        axios.delete(`http://localhost:8080/delete/${this.deleteUUID}`)
+        .then(() => this.closeDeleteDialog())
       },
-      editWorker(){
+      closeDeleteDialog(){
+        this.deleteDialog = false
+        this.getWorkers()
+        this.deleteUUID = -1
+      },
+      deleteWorker(item){
+        this.deleteUUID = item.uuid
+        this.deleteDialog = true
+      },
+      editWorker(item){
+        this.uuid = item.uuid
+        this.name = item.name
+        this.position = item.position
+        this.editDialog = true
+      },
+      editWorkerConfirm(){
         axios.post('http://localhost:8080/edit', { uuid: this.uuid, name: this.name, position: this.position })
-        .then(() => this.getWorkers())
+        .then(() => this.closeEditDialog())
+      },
+      closeEditDialog(){
+        this.editDialog = false
+        this.getWorkers()
+        this.uuid = ''
+        this.name = ''
+        this.position = ''
       }
     },
 
